3981f382bd1f1c383d0a68aac9591b4c26727f3e
omgthegreenranger <das.shaggy@gmail.com>
2023-02-16 22:51:34 -0500

    fetch GET working - refactored the fetch scripts

--- routes/notes.js
+++ routes/notes.js
@@ -1,10 +1,19 @@
 const notes = require('express').Router();
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const fs = require('fs');
 
 // GET Route to retrieve all notes
-notes.get('/', (req, res) =>
-    readFromFile('./db/db.json').then((data) => res.json(data))
-    );
+
+notes.get('/', function(req, res) {
+    // read the db.json file
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+
+      // parse the data and return all saved notes
+      const notes = JSON.parse(data);
+      res.json(notes);
+    });
+  });
 
 // POST Route for submitting new notes
 notes.post('/', (req, res) => {
@@ -17,7 +26,19 @@ notes.post('/', (req, res) => {
             text,
         };
 
-    readAndAppend(newNote, './db/db.json');
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const parsedData = JSON.parse(data);
+          parsedData.push(newNote);
+
+          fs.writeFile('./db/db.json', JSON.stringify(parsedData), (err) =>
+            err ? console.error(err) : console.info(`\nData written to db.json`)
+          );    
+        }
+      });
+
 
     const response = {
         status: 'Success',
--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
 
